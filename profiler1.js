0c7097a3707e1035f690b2c5a2453d37d1cc469a
SMTechnology133 <salmandac133@gmail.com>
2025-09-11 16:29:42 +0200

    Add files via upload

---
+++ profiler1.js
@@ -0,0 +1,19 @@
+//modal code
+$(document).ready(function() 
+{ 
+$("#myModal").modal('show'); 
+}); 
+
+$(document).ready(function(){
+$("h5").animate({
+color: "teal"
+});
+});
+
+$(document).ready (function(){
+$("img").attr("loading", "lazy");
+});
+
+rotatingSphere=()=>{
+	open("file:///storage/emulated/0/projects/ARmagic/rotatingSphere.html");
+}
